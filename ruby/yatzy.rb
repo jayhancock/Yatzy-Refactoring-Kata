dac5027865d8f9733d9b314c1f3bfc4551e345d0
Jay Hancock <jay@kibblessoftware.com>
2018-03-02 16:28:33 -0500

    Replace algorithm for chance, and update method signature to match new *dice pattern (flexibility)

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -1,21 +1,6 @@
 class Yatzy
-  def initialize(d1, d2, d3, d4, _5)
-    @dice = [0]*5
-    @dice[0] = d1
-    @dice[1] = d2
-    @dice[2] = d3
-    @dice[3] = d4
-    @dice[4] = _5
-  end
-
-  def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+  def self.chance(*dice)
+    dice.reduce(0, :+)
   end
 
   def self.yatzy(dice)
