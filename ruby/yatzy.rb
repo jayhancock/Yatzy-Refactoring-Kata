88ada1968ee2b0e305c145406927c0e3d8adf578
Jay Hancock <jay@kibblessoftware.com>
2018-03-02 16:02:42 -0500

    Start refactoring one ruleset - make into array (since later we want to vary the number of dice in the array)

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -24,19 +24,20 @@ class Yatzy
 
   def self.ones( d1,  d2,  d3,  d4,  d5)
     sum = 0
-    if (d1 == 1)
+    dice = [d1, d2, d3, d4, d5]
+    if (dice[0] == 1)
       sum += 1
     end
-    if (d2 == 1)
+    if (dice[1] == 1)
       sum += 1
     end
-    if (d3 == 1)
+    if (dice[2] == 1)
       sum += 1
     end
-    if (d4 == 1)
+    if (dice[3] == 1)
       sum += 1
     end
-    if (d5 == 1)
+    if (dice[4] == 1)
       sum += 1
     end
 
