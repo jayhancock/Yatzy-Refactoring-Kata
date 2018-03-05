bc6960089fa928f866bd514749a0557d57c38c9f
Jay Hancock <jay@kibblessoftware.com>
2018-03-04 19:36:28 -0500

    Since the test suite is in the format "these N dice and this method equal this result" I've rewritten the suite to be dry. 1) the previous suite method names were inconsistent 2) method of testing results was inconsistent 3) thoroughness of tests was lacking - some cases the score=0 case was not tested
    This also prepares for the testing of 4 dice as a variant.

--- ruby/test_yatzy.rb
+++ ruby/test_yatzy.rb
@@ -2,94 +2,71 @@ require_relative 'yatzy'
 require 'test/unit'
 
 class YatzyTest < Test::Unit::TestCase
-  def test_chance_scores_sum_of_all_dice
-    expected = 15
-    actual = Yatzy.chance(2,3,4,5,1)
-    assert expected == actual
-    assert 16 == Yatzy.chance(3,3,4,5,1)
-  end
+  TEST_DATA = [
+    [:chance, [2,3,4,5,1], 15],
+    [:chance, [3,3,4,5,1], 16],
+    [:yatzy,  [1,1,1,1,1], 50],
+    [:yatzy,  [2,2,2,2,2], 50],
+    [:yatzy,  [3,3,3,3,3], 50],
+    [:yatzy,  [4,4,4,4,4], 50],
+    [:yatzy,  [5,5,5,5,5], 50],
+    [:yatzy,  [6,6,6,6,6], 50],
+    [:yatzy,  [6,6,6,6,1], 0],
+    [:ones,   [1,2,3,4,5], 1],
+    [:ones,   [1,2,1,4,5], 2],
+    [:ones,   [1,2,1,1,1], 4],
+    [:ones,   [2,2,3,4,5], 0],
+    [:twos,   [1,2,3,4,5], 2],
+    [:twos,   [1,2,3,2,5], 4],
+    [:twos,   [2,2,3,2,2], 8],
+    [:twos,   [1,1,3,4,5], 0],
+    [:threes, [1,2,3,4,5], 3],
+    [:threes, [1,2,3,4,3], 6],
+    [:threes, [3,3,1,3,3], 12],
+    [:threes, [1,2,2,4,5], 0],
+    [:fours,  [1,2,3,4,5], 4],
+    [:fours,  [1,4,3,4,5], 8],
+    [:fours,  [4,4,3,4,4], 16],
+    [:fours,  [1,2,3,3,5], 0],
+    [:fives,  [1,2,3,4,5], 5],
+    [:fives,  [1,2,5,4,5], 10],
+    [:fives,  [5,5,5,4,5], 20],
+    [:fives,  [1,2,3,4,1], 0],
+    [:sixes,  [1,2,3,4,6], 6],
+    [:sixes,  [1,2,6,4,6], 12],
+    [:sixes,  [6,6,3,6,6], 24],
+    [:sixes,  [1,2,3,4,5], 0],
+    [:one_pair, [3,4,3,5,6], 6],
+    [:one_pair, [5,3,3,3,5], 10],
+    [:one_pair, [5,3,6,6,5], 12],
+    [:one_pair, [1,2,3,4,5], 0],
+    [:two_pair, [3,3,5,4,5], 16],
+    [:two_pair, [3,3,5,5,5], 16],
+    [:two_pair, [3,3,4,5,6], 0],
+    [:three_of_a_kind, [3,3,3,4,5], 9],
+    [:three_of_a_kind, [3,3,3,3,5], 9],
+    [:three_of_a_kind, [5,3,5,4,5], 15],
+    [:three_of_a_kind, [1,3,5,4,5], 0],
+    [:four_of_a_kind, [3,3,3,3,5], 12],
+    [:four_of_a_kind, [5,5,5,4,5], 20],
+    [:four_of_a_kind, [3,3,3,3,3], 12],
+    [:four_of_a_kind, [3,3,3,3,3], 12],
+    [:four_of_a_kind, [3,3,3,4,5], 0],
+    [:small_straight, [1,2,3,4,5], 15],
+    [:small_straight, [2,3,4,5,1], 15],
+    [:small_straight, [1,2,2,4,5], 0],
+    [:small_straight, [2,3,4,5,6], 0],
+    [:large_straight, [6,2,3,4,5], 20],
+    [:large_straight, [2,3,4,5,6], 20],
+    [:large_straight, [1,2,3,4,5], 0],
+    [:large_straight, [1,2,2,4,5], 0],
+    [:full_house, [6,2,2,2,6], 18],
+    [:full_house, [2,3,4,5,6], 0]
+  ]
 
-  def test_yatzy_scores_50
-    expected = 50
-    actual = Yatzy.yatzy(4,4,4,4,4)
-    assert expected == actual
-    assert 50 == Yatzy.yatzy(6,6,6,6,6)
-    assert 0 == Yatzy.yatzy(6,6,6,6,3)
-  end
-
-  def test_1s
-    assert Yatzy.ones(1,2,3,4,5) == 1
-    assert 2 == Yatzy.ones(1,2,1,4,5)
-    assert 0 == Yatzy.ones(6,2,2,4,5)
-    assert 4 == Yatzy.ones(1,2,1,1,1)
-  end
-
-  def test_2s
-    assert Yatzy.twos(1,2,3,2,6) == 4
-    assert Yatzy.twos(2,2,2,2,2) == 10
-  end
-
-  def test_threes
-    assert 6 == Yatzy.threes(1,2,3,2,3)
-    assert 12 == Yatzy.threes(2,3,3,3,3)
-  end
-
-  def test_fours_test
-    assert 12 == Yatzy.fours(4,4,4,5,5)
-    assert 8 == Yatzy.fours(4,4,5,5,5)
-    assert 4 == Yatzy.fours(4,5,5,5,5)
-  end
-
-  def test_fives()
-    assert 10 == Yatzy.fives(4,4,4,5,5)
-    assert 15 == Yatzy.fives(4,4,5,5,5)
-    assert 20 == Yatzy.fives(4,5,5,5,5)
-  end
-
-  def test_sixes_test
-    assert 0 == Yatzy.sixes(4,4,4,5,5)
-    assert 6 == Yatzy.sixes(4,4,6,5,5)
-    assert 18 == Yatzy.sixes(6,5,6,6,5)
-  end
-
-  def test_one_pair
-    assert 6 == Yatzy.one_pair (3,4,3,5,6)
-    assert 10 == Yatzy.one_pair(5,3,3,3,5)
-    assert 12 == Yatzy.one_pair(5,3,6,6,5)
-  end
-
-  def test_two_Pair
-    assert_equal 16, Yatzy.two_pair(3,3,5,4,5)
-    assert_equal 16, Yatzy.two_pair(3,3,5,5,5)
-  end
-
-  def test_three_of_a_kind()
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,4,5)
-    assert 15 == Yatzy.three_of_a_kind(5,3,5,4,5)
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,3,5)
-  end
-
-  def test_four_of_a_knd
-    assert 12 == Yatzy.four_of_a_kind(3,3,3,3,5)
-    assert 20 == Yatzy.four_of_a_kind(5,5,5,4,5)
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,3,3)
-    assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
-  end
-
-  def test_small_straight()
-    assert 15 == Yatzy.small_straight(1,2,3,4,5)
-    assert 15 == Yatzy.small_straight(2,3,4,5,1)
-    assert 0 == Yatzy.small_straight(1,2,2,4,5)
-  end
-
-  def test_large_straight
-    assert 20 == Yatzy.large_straight(6,2,3,4,5)
-    assert 20 == Yatzy.large_straight(2,3,4,5,6)
-    assert 0 == Yatzy.large_straight(1,2,2,4,5)
-  end
-
-  def test_full_house()
-    assert 18 == Yatzy.full_house(6,2,2,2,6)
-    assert 0 == Yatzy.full_house(2,3,4,5,6)
+  TEST_DATA.each do |method, dice, expected|
+    define_method("test_#{method}_#{dice.join}_#{expected}") do
+      assert Yatzy.send(method, *dice) == expected
+    end
   end
 end
